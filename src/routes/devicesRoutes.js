872fe424b0c0731f63f3ac3e041907e51d13c513
ash2kkid <ashwathpremchandh@gmail.com>
2025-12-12 22:35:55 +0530

    More Changes in devicesRoutes for middleware

--- src/routes/devicesRoutes.js
+++ src/routes/devicesRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { auth } from "../middleware/auth.js";
+import { authMiddleware } from "../middleware/auth.js";
 import { registerDevice, getMyDevices, sendCommand } from "../controllers/deviceController.js";
 
 const router = express.Router();
 
-router.post("/register", auth, registerDevice);
-router.get("/my", auth, getMyDevices);
-router.post("/cmd", auth, sendCommand);
+router.post("/register", authMiddleware, registerDevice);
+router.get("/my", authMiddleware, getMyDevices);
+router.post("/cmd", authMiddleware, sendCommand);
 
 export default router;
