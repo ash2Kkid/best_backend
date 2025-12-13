00ef70e764079972169c6339c81ffdd03fbb3070
ash2kkid <ashwathpremchandh@gmail.com>
2025-12-13 11:07:05 +0530

    Further fixes to the homeRoutes error

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { createHome, getMyHomes, inviteUser } from "../controllers/homeController.js";
-import authMiddleware from "../middleware/auth.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 // All routes require JWT auth
-router.use(authMiddleware);
+router.use(auth);
 
 // Create a home (Admin)
 router.post("/", createHome);
