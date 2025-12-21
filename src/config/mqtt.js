d887228ee5f45a1a37d49395fadc556159ed07e8
ash2kkid <ashwathpremchandh@gmail.com>
2025-12-21 10:55:57 +0530

    Fixed the error on MQTT

--- src/config/mqtt.js
+++ src/config/mqtt.js
@@ -2,7 +2,6 @@ import mqtt from "mqtt";
 import Device from "../models/Device.js";
 import { v4 as uuidv4 } from "uuid";
 
-export const pendingCommands = new Map();
 
 const MQTT_HOST = process.env.MQTT_HOST || "mqtt://broker.hivemq.com";
 
